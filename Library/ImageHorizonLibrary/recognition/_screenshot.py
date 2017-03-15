89f9da6ec369fc275c69eec473612e60d4a9734e
Kotchakon <kotchakon.ra@gmail.com>
2017-03-15 17:18:03 +0700

    update screenshoot

--- Library/ImageHorizonLibrary/recognition/_screenshot.py
+++ Library/ImageHorizonLibrary/recognition/_screenshot.py
@@ -11,14 +11,18 @@ from ..errors import ScreenshotFolderException
 
 
 class _Screenshot(object):
+    @property
     def _make_up_filename(self):
         try:
             suitename = BuiltIn().get_variable_value('${SUITE NAME}')
+            suitename = suitename.replace(' ','')
             suitename = suitename.split(".")
-            suitename = suitename[4].replace(' ','')
-            path = BuiltIn().get_variable_value('${TEST NAME}')
-            path = path.split("_")
-            path = 'screenshot-ImageHorizon-'+ suitename +'-%s' % path[0].replace(' ', '')
+            testname = BuiltIn().get_variable_value('${TEST NAME}')
+            testname = testname.split("_")
+            if len(suitename) > 1:
+                path = 'screenshot-ImageHorizon-' + suitename[4] + '-' + testname[0]
+            else:
+                path = 'screenshot-ImageHorizon-' + suitename[0] + '-'+ testname[0]
         except RobotNotRunningError:
             LOGGER.info('Could not get suite name, using '
                         'default naming scheme')
@@ -45,7 +49,7 @@ class _Screenshot(object):
         if not isinstance(target_dir, basestring):
             raise ScreenshotFolderException('Screenshot folder is invalid: '
                                             '"%s"' % target_dir)
-        path = self._make_up_filename()
+        path = self._make_up_filename
         path = abspath(path_join(target_dir, path))
         LOGGER.info('Screenshot taken: {0}<br/><img src="{0}" '
                     'width="100%" />'.format(path), html=True)
